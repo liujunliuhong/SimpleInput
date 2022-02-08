8fb880113027a1066f0823b512310d5f9c05a4a8
银河 <1035841713@qq.com>
2022-02-08 22:54:21 +0800

    modify to PerfectInput

--- PerfectInput/AppDelegate.swift
+++ PerfectInput/AppDelegate.swift
@@ -1,17 +1,17 @@
 //
 //  AppDelegate.swift
-//  LimitedInput
+//  PerfectInput
 //
-//  Created by jun on 2022/01/29.
+//  Created by galaxy on 2022/2/8.
 //
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.makeKeyAndVisible()
@@ -19,9 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let vc = ViewController(style: .plain)
         let navi = UINavigationController(rootViewController: vc)
         self.window?.rootViewController = navi
-        
         return true
     }
-
 }
 
--- Podfile
+++ Podfile
@@ -1,9 +1,19 @@
 # Uncomment the next line to define a global platform for your project
 platform :ios, '10.2'
-target 'LimitedInput' do
+
+target 'PerfectInput' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
-
   pod 'SnapKit'
+  # Pods for PerfectInput
+
+  target 'PerfectInputTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PerfectInputUITests' do
+    # Pods for testing
+  end
 
 end
